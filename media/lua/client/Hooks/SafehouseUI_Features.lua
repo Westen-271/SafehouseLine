9ac45b22a78f0c02a18a62fc24e77c5840bf8aff
Westen-271 <jack.robinson.work@gmail.com>
2025-06-07 07:55:50 +0100

    Add git ignore, add capability to add manager from button click.

--- media/lua/client/Hooks/SafehouseUI_Features.lua
+++ media/lua/client/Hooks/SafehouseUI_Features.lua
@@ -1,19 +1,33 @@
 require "ISUI/UserPanel/ISSafehouseUI";
-SafehouseClient = require("SafehouseClient"); 
+SafehouseClient = require("SafehouseClient");
 
 if isServer() then return end;
 
 local FONT_HGT_SMALL = getTextManager():getFontHeight(UIFont.Small);
 
 function ISSafehouseUI:onClickAddManager(button)
+    print("OnClickManager");
+    print(self.playerList.selected);
+
+    -- Find player and check if they are in game.
+    local selected = self.playerList.selected;
+
+    local selectedName = self.playerList.items[selected].item.name;
+    if not selectedName then return end;
+
+    -- Check if player is valid.
+    local matchingPlayer = getPlayerFromUsername(selectedName);
+    if not matchingPlayer then return end; -- They have likely DCed or crashed between selecting and clicking accept (rare edge case).
+
+    SafehouseClient.AddManagerToSafehouse(self.safehouse, matchingPlayer);
+    self:populateList();
 end 
 
 function ISSafehouseUI:onClickManagementPanel(button)
 end 
 
 function ISSafehouseUI:onMouseUp(x, y) -- NOTE, the self of this is the playerList itself!
-    print("YOU HAVE SELECTED PLAYER ");
-    print(self.selected);
+    print("SafehouseLine - onMouseUp");
 
     local selected = self.selected;
     if selected == 0 then return end;
@@ -21,9 +35,16 @@ function ISSafehouseUI:onMouseUp(x, y) -- NOTE, the self of this is the playerLi
     local selectedPlayer = self.items[selected].item;
     if not selectedPlayer then return end;
 
-    if not SafehouseClient.IsManagerEx(selectedPlayer.name, self.parent.safehouse) and self.parent:hasPrivilegedAccessLevel() then
+    print(selectedPlayer.name);
+
+    if not SafehouseClient.IsManagerEx(selectedPlayer.name, self.parent.safehouse) and self.parent:canAddManagers() then
+        print("Player is not manager and have permission to add manager.");
         self.parent.mgrBtn.enable = true;
     end
+end
+
+function ISSafehouseUI:canAddManagers()
+    return self:isOwner() or self:hasPrivilegedAccessLevel();
 end
 
 --[[
@@ -60,7 +81,7 @@ function ISSafehouseUI:initialise()
     ---
 
     -- Override vanilla functionality to allow managers to add players.
-    self.addPlayer.enable = self:isOwner() or hasPrivilegedAccessLevel or SafehouseClient.IsManager(getPlayer(), self.safehouse);
+    self.addPlayer.enable = self:isOwner() or self:hasPrivilegedAccessLevel() or SafehouseClient.IsManager(getPlayer(), self.safehouse);
 
     self.playerList.onMouseUp = self.onMouseUp;
 end
@@ -71,19 +92,19 @@ function ISSafehouseUI:populateList()
     self.playerList:clear();
 
     for i = 0, self.safehouse:getPlayers():size() - 1 do
-            local newPlayer = {};
-            newPlayer.name = self.safehouse:getPlayers():get(i);
+        local newPlayer = {};
+        newPlayer.name = self.safehouse:getPlayers():get(i);
+        newPlayer.isManager = SafehouseClient.IsManagerEx(newPlayer.name);
 
-            local lineStr = newPlayer.name;
+        local lineStr = newPlayer.name;
 
-            if SafehouseClient.IsManager(newPlayer) then
-                lineStr = lineStr .. (" - Manager");
-            end
+        if SafehouseClient.IsManager(newPlayer) then
+            lineStr = lineStr .. (" - Manager");
+        end
 
-            if newPlayer.name ~= self.safehouse:getOwner() or isDebugEnabled() then
-                self.playerList:addItem(lineStr, newPlayer);
-            end;
---        end
+        if newPlayer.name ~= self.safehouse:getOwner() or isDebugEnabled() then
+            self.playerList:addItem(lineStr, newPlayer);
+        end;
     end;
     self.playerList.selected = math.min(selected, #self.playerList.items);
 end
